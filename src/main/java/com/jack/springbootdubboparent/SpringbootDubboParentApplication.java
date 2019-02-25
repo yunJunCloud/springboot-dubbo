3c80dd61f837bbd10715a5009fa3200e76fdcee6
MakeJackM <34850263+MakeJackM@users.noreply.github.com>
2019-02-25 14:55:24 +0800

    Add files via upload

---
+++ src/main/java/com/jack/springbootdubboparent/SpringbootDubboParentApplication.java
@@ -0,0 +1,14 @@
+package com.jack.springbootdubboparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDubboParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDubboParentApplication.class, args);
+    }
+
+}
+
